2c376f15257d7fb98d0188a4568bada3773f6ef8
k1729p <116507738+k1729p@users.noreply.github.com>
2024-06-14 08:18:10 +0200

    done

--- recruitment_task/src/main/java/io/getint/recruitment_task/JiraSynchronizer.java
+++ recruitment_task/src/main/java/io/getint/recruitment_task/JiraSynchronizer.java
@@ -1,8 +1,35 @@
 package io.getint.recruitment_task;
 
-import java.io.IOException;
+import io.getint.recruitment_task.models.Issue;
+import io.getint.recruitment_task.services.Service;
+import io.getint.recruitment_task.utils.Helper;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.lang.invoke.MethodHandles;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.logging.Logger;
+
+import static io.getint.recruitment_task.Constants.*;
+
+/**
+ * The Jira synchronizer.
+ */
 public class JiraSynchronizer {
+    private static final Logger logger = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
+    private final Service service;
+
+    /**
+     * The constructor.
+     *
+     * @param service the service
+     */
+    public JiraSynchronizer(Service service) {
+        this.service = service;
+    }
+
     /**
      * Search for 5 tickets in one project, and move them
      * to the other project within same Jira instance.
@@ -11,7 +38,26 @@ public class JiraSynchronizer {
      * - description
      * - priority
      * Bonus points for syncing comments.
+     *
+     * @throws Exception the exception
      */
     public void moveTasksToOtherProject() throws Exception {
+
+        final JSONObject sourceProjectJSON = service.searchWithJql("project = " + SOURCE_PROJECT_KEY);
+        final List<Issue> issueList = Optional.of(Helper.loadIssueList(sourceProjectJSON))
+                .map(list -> list.subList(0, Math.min(list.size(), NUMBER_OF_MOVED_ISSUES)))
+                .orElse(List.of());
+        // add issues to target project
+        for (Issue issue : issueList) {
+            Optional.of(service.getComments(issue.key)).map(arg -> arg.getJSONArray("comments"))
+                    .filter(Predicate.not(JSONArray::isEmpty)).map(arg -> arg.getJSONObject(0))
+                    .map(arg -> arg.getString("body")).ifPresent(issue.commentsList::add);
+            service.createIssue(TARGET_PROJECT_KEY, issue);
+        }
+        // delete issues from source project
+        for (Issue issue : issueList) {
+            service.deleteIssue(issue.key);
+        }
+        logger.info(String.format("issue list size[%s]", issueList.size()));
     }
 }
--- recruitment_task/src/test/java/io/getint/recruitment_task/JiraSynchronizerTests.java
+++ recruitment_task/src/test/java/io/getint/recruitment_task/JiraSynchronizerTests.java
@@ -1,10 +1,199 @@
 package io.getint.recruitment_task;
 
+import io.getint.recruitment_task.models.Issue;
+import io.getint.recruitment_task.services.Service;
+import io.getint.recruitment_task.utils.Helper;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import static io.getint.recruitment_task.Application.PASSWORD;
+import static io.getint.recruitment_task.Constants.*;
+
+/**
+ * The Jira synchronizer tests.
+ */
 public class JiraSynchronizerTests {
+
+    private Service service;
+    private JiraSynchronizer jiraSynchronizer;
+    private static final boolean PROLIX = false;
+
+    /**
+     * Setups the test.
+     *
+     * @throws Exception the exception
+     */
+    @Before
+    public void setup() throws Exception {
+
+        service = new Service(PASSWORD);
+        jiraSynchronizer = new JiraSynchronizer(service);
+        if (PROLIX) {
+            Helper.reportProjects(service);
+        }
+        Helper.removeIssues(service);
+    }
+
+    /**
+     * Should sync tasks.
+     * The method 'jiraSynchronizer::moveTasksToOtherProject'
+     * is called three times in this test.
+     *
+     * @throws Exception the exception
+     */
     @Test
     public void shouldSyncTasks() throws Exception {
-        new JiraSynchronizer().moveTasksToOtherProject();
+
+        final AtomicInteger atomic = new AtomicInteger();
+        /*
+        test 1: added 7, actually moved 5, remained 2
+        */
+        // GIVEN
+        Helper.addIssuesToSourceProject(service, 7, atomic);
+        checkNumberOfIssuesInProjects(service, 7, 0);
+        // WHEN
+        jiraSynchronizer.moveTasksToOtherProject();
+        // THEN
+        checkNumberOfIssuesInProjects(service, 2,
+                NUMBER_OF_MOVED_ISSUES);
+        /*
+        test 2: added 2, actually moved 4, remained 0
+        */
+        // GIVEN
+        Helper.addIssuesToSourceProject(service, 2, atomic);
+        checkNumberOfIssuesInProjects(service, 4,
+                NUMBER_OF_MOVED_ISSUES);
+        // WHEN
+        jiraSynchronizer.moveTasksToOtherProject();
+        // THEN
+        checkNumberOfIssuesInProjects(service, 0,
+                2 * NUMBER_OF_MOVED_ISSUES - 1);
+        /*
+        test 3: added 5, actually moved 5, remained 0
+        */
+        // GIVEN
+        Helper.addIssuesToSourceProject(service, 5, atomic);
+        checkNumberOfIssuesInProjects(service, 5,
+                2 * NUMBER_OF_MOVED_ISSUES - 1);
+        // WHEN
+        jiraSynchronizer.moveTasksToOtherProject();
+        // THEN
+        checkNumberOfIssuesInProjects(service, 0,
+                3 * NUMBER_OF_MOVED_ISSUES - 1);
     }
+
+    /**
+     * Should sync single task.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void shouldSyncSingleTask() throws Exception {
+
+        // GIVEN
+        final AtomicInteger atomic = new AtomicInteger();
+        Helper.addIssuesToSourceProject(service, 1, atomic);
+        final JSONObject sourceProjectJSON = service.searchWithJql("project = " + SOURCE_PROJECT_KEY);
+        final List<Issue> sourceIssueList = Helper.loadIssueList(sourceProjectJSON);
+        Assert.assertEquals("source issues number", 1, sourceIssueList.size());
+        final Issue sourceIssue = sourceIssueList.get(0);
+        final String sourceComment = Optional.of(service.getComments(sourceIssue.key))
+                .map(arg -> arg.getJSONArray("comments"))
+                .filter(Predicate.not(JSONArray::isEmpty)).map(arg -> arg.getJSONObject(0))
+                .map(arg -> arg.getString("body")).orElse("");
+        // WHEN
+        jiraSynchronizer.moveTasksToOtherProject();
+        // THEN
+        checkNumberOfIssuesInProjects(service, 0, 1);
+        final JSONObject targetProjectJSON = service.searchWithJql("project = " + TARGET_PROJECT_KEY);
+        final Issue targetIssue = Helper.loadIssueList(targetProjectJSON).get(0);
+        final String targetComment = Optional.of(service.getComments(targetIssue.key))
+                .map(arg -> arg.getJSONArray("comments"))
+                .filter(Predicate.not(JSONArray::isEmpty)).map(arg -> arg.getJSONObject(0))
+                .map(arg -> arg.getString("body")).orElse("");
+        checkIssues(sourceIssue, sourceComment, targetIssue, targetComment);
+    }
+
+    /**
+     * Should execute without tasks to sync.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void shouldExecuteWithoutTasks() throws Exception {
+
+        // GIVEN
+        checkNumberOfIssuesInProjects(service, 0, 0);
+        // WHEN
+        jiraSynchronizer.moveTasksToOtherProject();
+        // THEN
+        checkNumberOfIssuesInProjects(service, 0, 0);
+    }
+
+    /**
+     * Checks number of issues in projects
+     *
+     * @param service              the service
+     * @param sourceIssuesExpected the  expected source issues
+     * @param targetIssuesExpected the  expected target issues
+     * @throws Exception the exception
+     */
+    private void checkNumberOfIssuesInProjects(
+            Service service, int sourceIssuesExpected, int targetIssuesExpected) throws Exception {
+
+        Function<JSONObject, Integer> projectJsonFun = projectJson ->
+                Optional.of(projectJson.getJSONArray("issues"))
+                        .map(arg -> {
+                            final AtomicInteger atomic = new AtomicInteger();
+                            for (int i = 0; i < arg.length(); i++) {
+                                Optional.of(arg.getJSONObject(i)).map(obj -> obj.getJSONObject("fields"))
+                                        .map(obj -> obj.getJSONObject("issuetype")).map(obj -> obj.get("name"))
+                                        .filter("Task"::equals).ifPresent(obj -> atomic.incrementAndGet());
+                            }
+                            return atomic.get();
+                        }).orElse(0);
+
+        final JSONObject sourceProjectJSON = service.searchWithJql("project = " + SOURCE_PROJECT_KEY);
+        Assert.assertEquals("source issues number", sourceIssuesExpected,
+                projectJsonFun.apply(sourceProjectJSON).intValue());
+        final JSONObject targetProjectJSON = service.searchWithJql("project = " + TARGET_PROJECT_KEY);
+        Assert.assertEquals("target issues number", targetIssuesExpected,
+                projectJsonFun.apply(targetProjectJSON).intValue());
+    }
+
+    /**
+     * Checks issues.
+     *
+     * @param sourceIssue   the source issue
+     * @param sourceComment the source comment
+     * @param targetIssue   the target issue
+     * @param targetComment the target comment
+     */
+    private void checkIssues(Issue sourceIssue, String sourceComment, Issue targetIssue, String targetComment) {
+
+        Assert.assertEquals("issueType", "Task", sourceIssue.issueType);
+        Assert.assertEquals("issueType", sourceIssue.issueType, targetIssue.issueType);
+        Assert.assertEquals("summary", "Summary Nr 1", sourceIssue.summary);
+        Assert.assertEquals("summary", sourceIssue.summary, targetIssue.summary);
+        Assert.assertEquals("description", "Description Nr 1", sourceIssue.description);
+        Assert.assertEquals("description", sourceIssue.description, targetIssue.description);
+        Assert.assertEquals("assignee", "", sourceIssue.assignee);
+        Assert.assertEquals("assignee", sourceIssue.assignee, targetIssue.assignee);
+        Assert.assertEquals("status", "To Do", sourceIssue.status);
+        Assert.assertEquals("status", sourceIssue.status, targetIssue.status);
+        Assert.assertEquals("priority", "Medium", sourceIssue.priority);
+        Assert.assertEquals("priority", sourceIssue.priority, targetIssue.priority);
+        Assert.assertEquals("comment", "Comment Nr 1", sourceComment);
+        Assert.assertEquals("comment", sourceComment, targetComment);
+    }
+
 }
