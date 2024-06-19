57c5137b93791e20857231239c8520fa4b9ed770
michalolejko <michal.olejko2@gmail.com>
2024-06-19 16:26:24 +0200

    Jira models

--- recruitment_task/src/main/java/io/getint/recruitment_task/model/Issue.java
+++ recruitment_task/src/main/java/io/getint/recruitment_task/model/Issue.java
@@ -1,4 +1,15 @@
 package io.getint.recruitment_task.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Issue {
+    private String id;
+    private String key;
+    private String summary;
+    private String description;
+    private Priority priority;
+    private String status;
 }
--- recruitment_task/src/main/java/io/getint/recruitment_task/model/Priority.java
+++ recruitment_task/src/main/java/io/getint/recruitment_task/model/Priority.java
@@ -1,4 +1,11 @@
 package io.getint.recruitment_task.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Priority {
+    private String id;
+    private String name;
 }
